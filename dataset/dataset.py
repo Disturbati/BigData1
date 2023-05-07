29d356cbe7aeb3299c58e520042a6a5a7d4da2bb
Davide Molitierno <>
2023-05-07 21:30:44 +0200

    Modifiche allo script per generare i dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -9,14 +9,22 @@ path = sys.path[0]
 # read csv in pandas dataframe
 df = pd.read_csv(path + '/Reviews.csv')
 
+
 random.seed(42)
+df_copy = df.copy()
+union_df = pd.DataFrame(columns=df.columns)
 
-for i in range(2, 11):
-    df['ProductId'] = df['ProductId'].apply(lambda x : ''.join(random.choice(string.ascii_uppercase  + string.digits) for _ in range(10)))
-    df['UserId'] = df['UserId'].apply(lambda x : ''.join(random.choice(string.ascii_uppercase  + string.digits) for _ in range(14)))
-    df['HelpfulnessDenominator'] = df['HelpfulnessDenominator'].transform(lambda x : random.randint(0, 100))
-    df['HelpfulnessNumerator'] = [random.randint(0, row['HelpfulnessDenominator']) for index, row in df.iterrows()]
-    df['Score'] = df['Score'].transform(lambda x : random.randint(1, 5))
-    df['Text'] = df['Text'].transform(lambda x : x[:random.randint(0, len(x))])
+steps = [2, 5, 10]
 
-    df.to_csv(path + '/Reviews' + str(i) + '.csv', index=False)
+for i in range(1, 10):
+    df_copy['ProductId'] = df_copy['ProductId'].apply(lambda x : ''.join(random.choice(string.ascii_uppercase  + string.digits) for _ in range(10)))
+    df_copy['UserId'] = df_copy['UserId'].apply(lambda x : ''.join(random.choice(string.ascii_uppercase  + string.digits) for _ in range(14)))
+    df_copy['HelpfulnessDenominator'] = df_copy['HelpfulnessDenominator'].transform(lambda x : random.randint(0, 100))
+    df_copy['HelpfulnessNumerator'] = [random.randint(0, row['HelpfulnessDenominator']) for index, row in df_copy.iterrows()]
+    df_copy['Score'] = df_copy['Score'].transform(lambda x : random.randint(1, 5))
+    df_copy['Text'] = df_copy['Text'].transform(lambda x : x[:random.randint(0, len(x))])
+
+    union_df = pd.concat([union_df, df_copy], ignore_index=True)
+
+    if i+1 in steps:
+        union_df.to_csv(path + '/Reviews_' + str(i+1) + '.csv', index=False)
